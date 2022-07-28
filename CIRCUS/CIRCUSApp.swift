0f8c959188599988132d9475dd87fc8d7cc1479a
nicoletanyt <nicole_tan_yitong@s2021.ssts.edu.sg>
2022-07-28 12:55:02 +0800

    Initial Commit

---
+++ CIRCUS/CIRCUSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CIRCUSApp.swift
+//  CIRCUS
+//
+//  Created by NICOLE TAN YITONG stu on 28/7/22.
+//
+
+import SwiftUI
+
+@main
+struct CIRCUSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CIRCUS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CIRCUS
+//
+//  Created by NICOLE TAN YITONG stu on 28/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
